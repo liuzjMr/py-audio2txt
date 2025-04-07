d4354d7f7d2d909630b7cb30ac6ae995498dbdc4
xuyd <sssxyd@gmail.com>
2025-04-07 11:18:05 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # py-audio2txt
-语音对话转文字, 如果是多人对话则检测speaker并按speaker输出，否则只输出转换的文字  
-在原语音文件相同位置生成转写文件，如 /path/to/some.mp3, 生成 /path/to/some.mp3.txt
+1. 语音对话转文字, 如果是多人对话则检测speaker并按speaker输出，否则只输出转换的文字  
+2. 在原语音文件相同位置生成转写文件，如 /path/to/some.mp3, 生成 /path/to/some.mp3.txt
+3. 可编辑 hotwords.txt 自定义热词
 
 # 依赖
 1. ffmpeg
@@ -34,13 +35,15 @@
     -h, --help      Show this help message
     -b, --batch     Batch size (default: 10)
     -l, --log-level Log level (default: INFO)
-    ```
+    -w, --hot-words Hot words file path (default: hotwords.txt)
+    --verbose   Verbose mode
+    ``` 
 
 # Models
 本项目依赖以下AI模型  
 首次执行会自动下载到 /home/your-user-dir/.cache/modelscope/hub/models/
 1. 降噪增强：[iic/speech_zipenhancer_ans_multiloss_16k_base](https://modelscope.cn/models/iic/speech_zipenhancer_ans_multiloss_16k_base)
-2. 语音识别：[iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch](https://modelscope.cn/models/iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch)
+2. 语音识别：[iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch](https://modelscope.cn/models/iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch)
 3. 端点检测: [iic/speech_fsmn_vad_zh-cn-16k-common-pytorch](https://modelscope.cn/models/iic/speech_fsmn_vad_zh-cn-16k-common-pytorch)
 4. 语音标点：[iic/punc_ct-transformer_cn-en-common-vocab471067-large](https://modelscope.cn/models/iic/punc_ct-transformer_cn-en-common-vocab471067-large)
 5. 说话人分离：[iic/speech_campplus_sv_zh-cn_16k-common](https://modelscope.cn/models/iic/speech_campplus_sv_zh-cn_16k-common)
--- audio2txt.py
+++ audio2txt.py
@@ -5,7 +5,7 @@ import tempfile
 from typing import Optional
 from modelscope.pipelines import pipeline
 from pydub import AudioSegment
-from common import load_args
+from common import get_executable_directory, load_args
 from modelscope.utils.constant import Tasks
 
 # 配置日志
@@ -16,20 +16,35 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+def load_hot_words(hot_words_file : str) -> list[str]:
+    """加载热词"""
+    if not os.path.exists(hot_words_file):
+        logger.warning(f"热词文件 {hot_words_file} 不存在，使用默认热词")
+        return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    hot_words = []
+    with open(hot_words_file, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) > 0 and not line.startswith("#"):
+                hot_words.append(line)
+    return hot_words
+
 def load_modelscope_pipelines() -> tuple[pipeline, pipeline]:
     # 语音降噪
     enhancer_pipe = pipeline(
         Tasks.acoustic_noise_suppression, 
         model='iic/speech_zipenhancer_ans_multiloss_16k_base'
     )
-
     # 通话转写
     inference_pipeline = pipeline(
         task=Tasks.auto_speech_recognition,
-        model='iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch', model_revision='v2.0.5', # 语音识别
+        # model='iic/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404',  model_revision='v2.0.5', # 语音识别
+        model='iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch', model_revision='v2.0.5', # 语音识别
+        # model='iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch', model_revision='v2.0.5', # 语音识别
         vad_model='iic/speech_fsmn_vad_zh-cn-16k-common-pytorch', vad_model_revision="v2.0.4",      # 语音端点检测
         punc_model='iic/punc_ct-transformer_cn-en-common-vocab471067-large', punc_model_revision="v2.0.4", # 语音标点
-        spk_model="iic/speech_campplus_sv_zh-cn_16k-common", spk_model_revision="v2.0.2", # 说话人分离
+        spk_model="iic/speech_campplus_sv_zh-cn_16k-common", spk_model_revision="v2.0.2", # 说话人分离 
     )    
     return enhancer_pipe, inference_pipeline
 
@@ -95,11 +110,22 @@ def enhance_wav(enhancer_pipe : pipeline, input_path: str) -> str:
         logger.warning(f"使用原始音频文件替代: {input_path} -> {enhanced_path}")
         return input_path
     
+def mill2minutes(millseconds: int) -> str:
+    remain_secondes = int(millseconds % 1000)
+    total_seconds = int(millseconds // 1000)
+    remain_seconds = int(total_seconds % 60)
+    total_minutes = int(total_seconds // 60)
+    return f"{total_minutes:02d}:{remain_seconds:02d}.{remain_secondes:03d}"    
       
-def transcript_wavs(inference_pipeline : pipeline, wav_files: list[str]) -> list[dict[str, str]]:
+def transcript_wavs(inference_pipeline : pipeline, hotword : str, wav_files: list[str], verbose: bool) -> list[dict[str, str]]:
     wav_contents = []
     try:
-        rec_result = inference_pipeline(wav_files, batch_size_s=300, batch_size_token_threshold_s=40)
+        rec_result = inference_pipeline(
+            wav_files, 
+            batch_size_s=300, 
+            batch_size_token_threshold_s=40,
+            hotword=hotword,
+        )
         for i, item in enumerate(rec_result):
             if item['sentence_info'] is None:
                 wav_contents.append({"wav": wav_files[i], "content": ""})
@@ -113,7 +139,8 @@ def transcript_wavs(inference_pipeline : pipeline, wav_files: list[str]) -> list
                 txt = item['text']
                 if spk is None or txt is None or len(txt) == 0:
                     continue
-                lines.append({'spk': spk, 'text': txt})
+                start = int(item['start'])
+                lines.append({'spk': spk, 'text': txt, 'start': start})
                 speakers.add(spk)    
             
             if len(speakers) < 2:
@@ -123,22 +150,30 @@ def transcript_wavs(inference_pipeline : pipeline, wav_files: list[str]) -> list
 
             pre_speaker = None
             pre_line = ""
+            pre_start = 0
             contents = []
             for line in lines:
                 current_speaker = line['spk']
                 current_line = line['text']
+                current_start = line['start']
                 if pre_speaker is None:
                     pre_speaker = current_speaker
                     pre_line = current_line
+                    pre_start = current_start
                 elif pre_speaker != current_speaker:
-                    contents.append(f"Speaker_{pre_speaker}: {pre_line}")
+                    contents.append(f"Speaker_{pre_speaker} {mill2minutes(pre_start)}: {pre_line}")
                     pre_speaker = current_speaker
                     pre_line = current_line
+                    pre_start = current_start
                 else:
                     pre_line += current_line
             if pre_speaker is not None:
-                contents.append(f"Speaker_{pre_speaker}: {pre_line}")
+                contents.append(f"Speaker_{pre_speaker} {mill2minutes(pre_start)}: {pre_line}")
             wav_contents.append({"wav": wav_files[i], "content": "\n".join(contents)})
+            if verbose:
+                logger.info(f"音频文件 {wav_files[i]} 转写结果：\n{wav_contents[-1]['content']}")
+            else:
+                logger.debug(f"音频文件 {wav_files[i]} 转写结果：\n{wav_contents[-1]['content']}")
         return wav_contents
     except Exception as e:
         logger.error(f"转写失败: {str(e)}")
@@ -169,7 +204,7 @@ def save_transcript(original_path: str, content: str):
         f.write(content)
     logger.debug(f"转写结果已保存至：{txt_path}")
 
-def process_batch(enhancer_pipe, inference_pipeline : pipeline, file_batch: list[str]) -> int:
+def process_batch(enhancer_pipe, inference_pipeline : pipeline, hotword: str, file_batch: list[str], verbose: bool) -> int:
     count = 0
     try:
         # 预处理阶段
@@ -194,7 +229,7 @@ def process_batch(enhancer_pipe, inference_pipeline : pipeline, file_batch: list
                     os.remove(wav_path)
         
         # 批量转写
-        transcripts = transcript_wavs(inference_pipeline, preprocessed_files)
+        transcripts = transcript_wavs(inference_pipeline, hotword, preprocessed_files, verbose)
         logger.debug(f"批量转写完成，共处理 {len(preprocessed_files)} 个文件")
         logger.debug("转写结果：")
         for transcript in transcripts:
@@ -221,7 +256,7 @@ def process_batch(enhancer_pipe, inference_pipeline : pipeline, file_batch: list
         return count
 
 
-def main(input_paths: list[str], batch_size: int = 10):
+def main(input_paths: list[str], batch_size: int, hot_words_file: str, verbose: bool):
     """主处理流程"""
     audio_files = collect_audio_files(input_paths)
     if len(audio_files) == 0:
@@ -231,13 +266,18 @@ def main(input_paths: list[str], batch_size: int = 10):
     logger.info(f"共发现 {len(audio_files)} 个音频文件")
 
     # 加载模型
+    hot_words = load_hot_words(hot_words_file)
+    hotword = '' if len(hot_words) == 0 else ' '.join(hot_words)
     enhancer_pipe, inference_pipeline = load_modelscope_pipelines()
+    if verbose:
+        logger.info("hotword: " + hotword)
+    logger.info("模型加载完成")
 
     total = 0
     for i in range(0, len(audio_files), batch_size):
         batch = audio_files[i:i+batch_size]
         logger.info(f"\n正在处理第 {i//batch_size +1} 批文件（{len(batch)}个）")
-        count = process_batch(enhancer_pipe, inference_pipeline, batch)
+        count = process_batch(enhancer_pipe, inference_pipeline, hotword, batch, verbose)
         total += count
         logger.info(f"第 {i//batch_size +1} 批（{len(batch)}个）文件处理完成, 共转写 {count} 个文件")
     logger.info(f"所有{len(audio_files)}个文件处理完成, 共转写 {total} 个文件")
@@ -259,11 +299,18 @@ if __name__ == "__main__":
         print("  -h, --help      Show this help message")
         print("  -b, --batch     Batch size (default: 10)")
         print("  -l, --log-level Log level (default: INFO)")
+        print("  -w, --hot-words Hot words file path (default: hotwords.txt)")
+        print("  --verbose   Verbose mode")
         exit(0)    
     batch_size = int(options.get("b", options.get("batch", 10)))
     log_level = options.get("l", options.get("log-level", "INFO")).upper()
+    hot_words_file = options.get("w", options.get("hot-words", ""))
+    if hot_words_file == "":
+        hot_words_file = os.path.join(get_executable_directory(), "hotwords.txt") 
+
+    verbose = options.get("verbose", False)
     logging.getLogger().setLevel(getattr(logging, log_level, logging.INFO))
     logger.info(f"当前日志级别: {log_level}")
     logger.info(f"当前批处理大小: {batch_size}")
-
-    main(input_paths=params, batch_size=batch_size)
+    logger.info(f"当前热词文件: {hot_words_file}")
+    main(input_paths=params, batch_size=batch_size, hot_words_file=hot_words_file, verbose=verbose)
